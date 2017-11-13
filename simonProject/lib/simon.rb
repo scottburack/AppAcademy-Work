e10bcce2f092143fb60aee5c36f57e95d0f2531b
scottburack <32395163+scottburack@users.noreply.github.com>
2017-11-12 23:02:45 -0500

    Simon Project

---
+++ simonProject/lib/simon.rb
@@ -0,0 +1,70 @@
+class Simon
+  COLORS = %w(red blue green yellow)
+
+  attr_accessor :sequence_length, :game_over, :seq
+
+  def initialize
+    @sequence_length = 1
+    @game_over = false
+    @seq = []
+  end
+
+  def play
+    take_turn until @game_over
+    if @game_over
+      game_over_message
+      reset_game
+    end
+  end
+
+  def take_turn
+    show_sequence
+    require_sequence
+
+    unless @game_over
+      round_success_message
+      @sequence_length += 1
+    end
+  end
+
+  def show_sequence
+    add_random_color
+    @seq.each do |color|
+      puts color
+      sleep 0.75
+      system("clear")
+      sleep 0.25
+    end
+
+  end
+
+  def require_sequence
+    puts "Repeat the sequence by entering the first letter of each color on a new line."
+    @seq.each do |color|
+      user_color = gets.chomp
+      if color[0] != user_color
+        @game_over = true
+        break
+      end
+    end
+    sleep 0.25
+  end
+
+  def add_random_color
+    seq << COLORS[rand(0..3)]
+  end
+
+  def round_success_message
+    "Round was a success. Here's is the next sequence:"
+  end
+
+  def game_over_message
+    p "Game over!"
+  end
+
+  def reset_game
+    @sequence_length = 1
+    @game_over = false
+    @seq = []
+  end
+end
